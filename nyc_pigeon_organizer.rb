376eb0036c9f357f9a62642f040d50bbb82eec7c
Ryan Werner <rmwerner@umich.edu>
2020-06-21 20:33:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,44 +1,36 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  pigeons_symbols = []
   pigeons_hash = {}# write your code here!
   pigeons = [data[:gender][:male], data[:gender][:female]].flatten()
-  pigeons.each do |element|
-    pigeons_symbols << ":#{element}"
-  end
-  pigeons_strings = []
-  pigeons_symbols.each do |element|
-    pigeons_strings << element.to_s
-  end
   count = 0
-  while count < pigeons_symbols.length do
-    pigeons_hash.merge!(pigeons_strings[count] => {:color => [], :gender => [], :lives => []})
+  while count < pigeons.length do
+    pigeons_hash.merge!(pigeons[count] => {:color => [], :gender => [], :lives => []})
     count += 1
   end
   colors = data[:color].keys
   genders = data[:gender].keys
   lives = data[:lives].keys
   count1 = 0
-  while count1 < pigeons_symbols.length do
+  while count1 < pigeons.length do
     count2 = 0
     while count2 < colors.length do
-      if data[:color][colors[count2]].include?(pigeons_strings[count1])
-        pigeons_hash[pigeons_strings[count1]][:color] << data[:color][colors[count2]]
+      if data[:color][colors[count2]].include?(pigeons[count1])
+        pigeons_hash[pigeons[count1]][:color] << data[:color][colors[count2]]
       end
       count2 += 1
     end
     count3 = 0
     while count3 < genders.length do
-      if data[:gender][genders[count3]].include?(pigeons_strings[count1])
-        pigeons_hash[pigeons_strings[count1]][:gender] << data[:gender][genders[count3]]
+      if data[:gender][genders[count3]].include?(pigeons[count1])
+        pigeons_hash[pigeons[count1]][:gender] << data[:gender][genders[count3]]
       end
       count3 += 1
     end
     count4 = 0
     while count4 < lives.length do
-      if data[:lives][lives[count4]].include?(pigeons_strings[count1])
-        pigeons_hash[pigeons_strings[count1]][:lives] << data[:lives][lives[count4]]
+      if data[:lives][lives[count4]].include?(pigeons[count1])
+        pigeons_hash[pigeons[count1]][:lives] << data[:lives][lives[count4]]
       end
       count4 += 1
     end
